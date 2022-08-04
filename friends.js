f7384677e7cc41b6c1d9349627e5cef076dab060
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-08-04 13:21:26 -0600

    Initial commit

---
+++ friends.js
@@ -0,0 +1,9 @@
+var number = [34, 67, 78, 21, 2, 12, 7, 88];
+
+var arrayLength = number.length;
+
+for (var i = 0; i < arrayLength; i++) {
+  if (number[i] > 30) {
+    console.log(number[i]);
+  }
+};
