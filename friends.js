d7da6dd15d864199ba9343a8bd0c022b7f07f819
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-08-04 13:29:51 -0600

    Add other method

--- friends.js
+++ friends.js
@@ -7,3 +7,9 @@ for (var i = 0; i < arrayLength; i++) {
     console.log(number[i]);
   }
 };
+var arrayLength = number.length;
+for (var i = 0; i < arrayLength; i++) {
+  if (number[i] < 60) {
+    console.log(number[i]);
+  }
+}
